f15ad80d99ad8ad7811c96d1693f7926e8e789e5
erosdfp <eros.daniel@fizetesipont.hu>
2024-09-26 14:43:41 +0200

    Add staging table to DML

--- data/queries/migration/create_monitoring_table.sql
+++ data/queries/migration/create_monitoring_table.sql
@@ -8,5 +8,19 @@ CREATE TABLE [WORK].[dbo].[T_HealthCheckResult] (
     [IsHealthy]          BIT            NOT NULL,
     [Query]              VARCHAR (1000) NOT NULL,
     [Column]             VARCHAR (30)   NOT NULL,
+    [ErrorMessage]       NVARCHAR (MAX) NULL,
     CONSTRAINT [PK_T_HealthCheckResult_RunDate_Table_Database_MetricType_IsHealthy_Column] PRIMARY KEY CLUSTERED ([RunDate] ASC, [Table] ASC, [Database] ASC, [MetricType] ASC, [Column] ASC, [IsHealthy] ASC, [Column] ASC)
+);
+
+CREATE TABLE [WORK].[dbo].[T_HealthCheckResult_Staging] (
+    [RunDate]            DATE           NULL,
+    [Table]              VARCHAR (30)   NULL,
+    [Database]           VARCHAR (30)   NULL,
+    [MetricType]         VARCHAR (30)   NULL,
+    [AbsoluteDifference] INT            NULL,
+    [PercentDifference]  DECIMAL (5, 3) NULL,
+    [IsHealthy]          BIT            NULL,
+    [Query]              VARCHAR (1000) NULL,
+    [Column]             VARCHAR (30)   NULL,
+    [ErrorMessage]       NVARCHAR (MAX) NULL,
 );
